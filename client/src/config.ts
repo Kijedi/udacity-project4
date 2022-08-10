575dd6d89cd1f3ddcfa9e8911a0102b7eb7db850
wkijedi <wesleigh#1>
2022-08-10 15:24:19 +0300

    Updated the model schema for handlers

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 're012fx4xi'
+const apiId = 'kx2stjerc3'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
